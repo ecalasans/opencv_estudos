0eb98dea79dcbd717b79bb4f7ce78569462059c1
ericcalasans <ericcalasans@gmail.com>
2020-03-27 08:33:19 -0300

    Documentação dos métodos

--- jupytercv.py
+++ jupytercv.py
@@ -1,13 +1,27 @@
-# Módulo com funções para auxiliar no manejo
-# do OpenCV em notebooks jupyter
+'''
+Módulo com funções para auxiliar no manejo
+do OpenCV em notebooks jupyter
+
+por Eric Calasans
+20/03/2020
+'''
 
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''
+Método imshoGrayscale - mostra a imagem em escala de cinza utilizando
+Matplotilb
+
+Parâmetros:  matImage - matriz obtida pelo método cv2.imread do OpenCV ou
+    uma matriz do tipo np.array
+'''
 def imshowGrayscale(matImage):
+    #Converte o modo de cor de BGR para RGB - modo de cor utilizado pelo Matplotlib
     rgb = cv2.cvtColor(matImage, cv2.COLOR_BGR2RGB)
 
+    #Plota a imagem
     ax = plt.gca()
     ax.imshow(rgb[:, :, 1], cmap='gray', interpolation='none', origin='upper')
 
@@ -15,7 +29,20 @@ def imshowGrayscale(matImage):
 
     plt.show()
 
+
+'''
+Método extractRegion - extrai uma região de uma imagem definida
+por um retângulo 
+
+Parâmetros:  imagem - matriz obtida pelo método cv2.imread do OpenCV ou
+    uma matriz do tipo np.array
+             region - lista contendo coordenadas do canto superior esquerdo e
+    inferior direito do tipo [(x1,y1), (x2,y2)]
+
+Retorno:  np.array (abs(x2-x1), abs(y2-y1))
+'''
 def extractRegion(imagem, region):
+    # Constrói o retângulo desejado
     x_i = min(region[0][0], region[1][0])
     x_f = max(region[0][0], region[1][0])
 
