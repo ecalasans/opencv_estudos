a6ff29293ff997f690b19b007c8039bacfcfe6f7
ericcalasans <ericcalasans@gmail.com>
2020-04-15 10:25:21 -0300

    Constructing Basic Shapes in OpenCV - Consegui desenhar vários círculos

--- eventomouse.py
+++ eventomouse.py
@@ -11,9 +11,14 @@ def pegaPixel(event, x, y, flags, param):
         print("Posição x = {} e y = {}".format(y, x))
         print("Cor do pixel = {}".format(swars[y,x]))
         cv2.circle(swars, (x,y), 10, 255, 1, 8, 0)
-        cv2.imshow('imagem', swars)
+        cv2.imshow('starwars', swars)
 
-cv2.imshow('starwars', swars)
+cv2.namedWindow('starwars')
 cv2.setMouseCallback('starwars', pegaPixel)
-cv2.waitKey(0)
+
+while True:
+    cv2.imshow('starwars', swars)
+    if cv2.waitKey(0):
+        break
+
 cv2.destroyAllWindows()
