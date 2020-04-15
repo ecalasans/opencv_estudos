c742ebbd92ba39034fa17eee7e1cfa364efe7dab
ericcalasans <ericcalasans@gmail.com>
2020-04-15 10:21:00 -0300

    Constructing Basic Shapes in OpenCV - Consigo desenhar apenas um círculo

--- eventomouse.py
+++ eventomouse.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
+import constant as ct
 
 swars = cv2.imread('starwars.jpg')
+img = np.zeros((400,500,1), dtype='uint8')
+img[:] = 125
 
 def pegaPixel(event, x, y, flags, param):
-    global mx, my
-    if event == cv2.EVENT_LBUTTONDOWN:
-       print("Posição x = {} e y = {}".format(x, y))
-       print("Cor do pixel = {}".format(swars[y,x]))
+    if event == cv2.EVENT_LBUTTONUP:
+        print("Posição x = {} e y = {}".format(y, x))
+        print("Cor do pixel = {}".format(swars[y,x]))
+        cv2.circle(swars, (x,y), 10, 255, 1, 8, 0)
+        cv2.imshow('imagem', swars)
 
 cv2.imshow('starwars', swars)
 cv2.setMouseCallback('starwars', pegaPixel)
