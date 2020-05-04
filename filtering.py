9ebf3a69d7f742105e967127a57894e265fd354e
ericcalasans <ericcalasans@gmail.com>
2020-05-04 16:26:19 -0300

    Image Processing Techniques - Image Filtering completo

--- filtering.py
+++ filtering.py
@@ -3,13 +3,33 @@ import numpy as np
 
 swars = cv2.imread('starwars.jpg')
 
-#Blurring
+###   Blurring   ###
 #  Aplicando kernel próprio
 #bk = np.ones((5,5), np.float32)/25
 #img = cv2.filter2D(swars, -1, bk)
 
 #  Aplicando funções nativas
-img_b = cv2.blur(swars, (10,10))
+#img_b = cv2.blur(swars, (10,10))
+#cv2.imshow('blur', img_b)
 
-cv2.imshow('blur', img)
+#  Gaussian blur
+#img_g = cv2.GaussianBlur(swars, (9,9), 0)
+#cv2.imshow('blur', img_g)
+
+#  Median blur
+#img_med = cv2.medianBlur(swars, 25)
+#cv2.imshow('blur', img_med)
+
+#  Bilateral blur
+#img_bil = cv2.bilateralFilter(swars, 5, 150, 150)
+cv2.imshow('normal', swars)
+#cv2.imshow('blur', img_bil)
+
+
+
+### Sharpening  ###
+# Unsharp masking - uma versão smoothed é subtraída da imagem original
+smoothed = cv2.GaussianBlur(swars, (9,9), 10)
+unsharped = cv2.addWeighted(swars, 1.5, smoothed, -0.5, 0)
+
 cv2.waitKey(0)
