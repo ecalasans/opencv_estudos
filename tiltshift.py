ecdfb06a8f38f88728dbdb21e3e0ba97ada2230f
ericcalasans <ericcalasans@gmail.com>
2020-11-18 17:33:35 -0300

    tiltshift.py - projetando deslocamento em bloco

--- tiltshift.py
+++ tiltshift.py
@@ -10,6 +10,9 @@ imagem = cv.imread('ts_480.jpg')
 im_h = imagem.shape[0]
 im_w = imagem.shape[1]
 im_h_meio = np.int(im_h/2)
+linha_sup = 0
+linha_inf = 0
+d = 0
 
 # Canvas para plotagem das imagens
 # Dimensões do canvas
@@ -23,27 +26,79 @@ cv.namedWindow('Tilt Shift')
 # Ajusta altura da faixa em foco
 def ajustaAltura(valor):
     # Calcula a altura das linhas superior e inferior
+    global linha_sup, linha_inf
+
     linha_sup = im_h_meio - valor
     linha_inf = im_h_meio + valor
 
-    # Desenha as linhas na imagem
+    # Desenha as linhas na imagem e a atualiza com a posição das mesmas
     cv.line(canvas, (0, linha_sup), (im_w, linha_sup), color=(0,0,255),
             thickness=1, lineType=cv.LINE_AA)
+    cv.line(canvas, (0, linha_inf), (im_w, linha_inf), color=(0, 255, 0),
+            thickness=1, lineType=cv.LINE_AA)
 
-    # Atualiza a imagem
     cv.imshow("Tilt Shift", canvas)
+
     canvas[:, 0:480, :] = imagem
     canvas[:, 481:961, :] = 180
+
     cv.line(canvas, (0, linha_sup), (im_w, linha_sup), color=(0, 0, 255),
             thickness=1, lineType=cv.LINE_AA)
+    cv.line(canvas, (0, linha_inf), (im_w, linha_inf), color=(0, 255, 0),
+            thickness=1, lineType=cv.LINE_AA)
+
     cv.imshow("Tilt Shift", canvas)
 
 # Ajusta força do decaimento
 def ajustaForca(valor):
-    pass
+    global d
+    d = valor
+
 # Ajusta posição vertical do desfoque
 def ajustaPosicao(valor):
-    pass
+    global im_h_meio, linha_sup, linha_inf
+
+    im_h_meio = valor
+    linha_sup = im_h_meio - valor
+    linha_inf = im_h_meio + valor
+
+    # Desloca as linhas em bloco para cima ou para baixo
+    if valor <= 180:
+        # Desenha as linhas na imagem e a atualiza com a posição das mesmas
+        cv.line(canvas, (0, linha_sup-valor), (im_w, linha_sup-valor),
+                color=(0, 0, 255), thickness=1, lineType=cv.LINE_AA)
+        cv.line(canvas, (0, linha_inf-valor), (im_w, linha_inf-valor),
+                color=(0, 255, 0), thickness=1, lineType=cv.LINE_AA)
+
+        cv.imshow("Tilt Shift", canvas)
+
+        canvas[:, 0:480, :] = imagem
+        canvas[:, 481:961, :] = 180
+
+        cv.line(canvas, (0, linha_sup-valor), (im_w, linha_sup-valor),
+                color=(0, 0, 255), thickness=1, lineType=cv.LINE_AA)
+        cv.line(canvas, (0, linha_inf-valor), (im_w, linha_inf-valor),
+                color=(0, 255, 0), thickness=1, lineType=cv.LINE_AA)
+
+        cv.imshow("Tilt Shift", canvas)
+    else:
+        # Desenha as linhas na imagem e a atualiza com a posição das mesmas
+        cv.line(canvas, (0, linha_sup+valor), (im_w, linha_sup+valor), color=(0, 0, 255),
+                thickness=1, lineType=cv.LINE_AA)
+        cv.line(canvas, (0, linha_inf+valor), (im_w, linha_inf+valor), color=(0, 255, 0),
+                thickness=1, lineType=cv.LINE_AA)
+
+        cv.imshow("Tilt Shift", canvas)
+
+        canvas[:, 0:480, :] = imagem
+        canvas[:, 481:961, :] = 180
+
+        cv.line(canvas, (0, linha_sup+valor), (im_w, linha_sup+valor), color=(0, 0, 255),
+                thickness=1, lineType=cv.LINE_AA)
+        cv.line(canvas, (0, linha_inf+valor), (im_w, linha_inf+valor), color=(0, 255, 0),
+                thickness=1, lineType=cv.LINE_AA)
+
+        cv.imshow("Tilt Shift", canvas)
 
 # Criação dos controles
 # Trackbar para tamanho da faixa de foco
@@ -53,12 +108,12 @@ cv.createTrackbar("Janela de Foco", "Tilt Shift", 0, im_h_meio, ajustaAltura)
 cv.createTrackbar("Forca do decaimento(d)", "Tilt Shift", 0, 100, ajustaForca)
 
 # Trackbar para ajustar a posição vertical da janela de foco
-cv.createTrackbar("Posição da Janela", "Tilt Shift", 0, im_h_meio, ajustaPosicao)
+cv.createTrackbar("Posicao na Janela", "Tilt Shift", im_h_meio, im_h, ajustaPosicao)
 
 ########################################################################
 # Programação
 ########################################################################
-
+print(linha_inf, linha_sup)
 
 cv.imshow('Tilt Shift', canvas)
 cv.waitKey(0)
