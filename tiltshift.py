a3d94dcb2565a5c1472a3cede22148764440ccee
ericcalasans <ericcalasans@gmail.com>
2020-11-19 16:17:51 -0300

    tiltshift.py - implementando a funcao tiltshift

--- tiltshift.py
+++ tiltshift.py
@@ -7,21 +7,39 @@ import scipy as sc
 ########################################################################
 # Imagem utilizada
 imagem = cv.imread('ts_480.jpg')
+
+# Filtro da média
+media = np.ones((5,5), dtype=np.float32)/25
+
+# Imagem após aplicação do filtro da média
+imagem_media = cv.filter2D(imagem, None, media)
+
+# Dimensões da imagem
 im_h = imagem.shape[0]
 im_w = imagem.shape[1]
-im_h_meio = np.int(im_h/2)
-pivo = im_h_meio
-linha_sup = 0
-linha_inf = 0
-d = 0
+im_h_meio = np.int(im_h/2)    # Ponto médio do eixo x
+
+# Inicialização de variáveis de interesse
+pivo = im_h_meio   #  Ponto médio entre as linhas
+linha_sup = 0   # Limite superior da região de foco
+linha_inf = 0   # Limite inferior da região de foco
+d = 0   # Força do decaimento
 
 # Canvas para plotagem das imagens
 # Dimensões do canvas
 canvas = np.zeros((im_h, (2*im_w) + 1, 3), dtype='uint8')
 canvas[:, 0:480, :] = imagem
-canvas[:, 481:961, :] = 180
+canvas[:, 481:961, :] = imagem_media
 cv.namedWindow('Tilt Shift')
 
+# Função de ponderação(calcula alfa e 1-alfa)
+def alfa(ls, li, d):
+    x = np.zeros((im_h, im_w, 3), dtype=np.float)
+
+    uns = np.ones((im_h, im_w, 3), dtype=np.float)
+    alfa = (np.tanh((x-ls)/d) - np.tanh((x-li)/d))*0.5
+    um_menos_alfa = np.subtract(uns, alfa)
+    return alfa, um_menos_alfa
 
 #Funções para trackbars
 # Ajusta altura da faixa em foco
@@ -31,7 +49,6 @@ def ajustaAltura(valor):
     pivo = cv.getTrackbarPos('Posicao na Janela', 'Tilt Shift')
     linha_sup = pivo - valor
     linha_inf = pivo + valor
-    print(pivo)
 
     # Desenha as linhas na imagem e a atualiza com a posição das mesmas
     cv.line(canvas, (0, linha_sup), (im_w, linha_sup), color=(0,0,255),
@@ -42,7 +59,12 @@ def ajustaAltura(valor):
     cv.imshow("Tilt Shift", canvas)
 
     canvas[:, 0:480, :] = imagem
-    canvas[:, 481:961, :] = 180
+
+    if d == 0:
+        canvas[:, 481:961, :] = imagem_media
+    else:
+        efeito = tiltShift(linha_sup, linha_inf, d)
+        canvas[:, 481:961, :] = efeito
 
     cv.line(canvas, (0, linha_sup), (im_w, linha_sup), color=(0, 0, 255),
             thickness=1, lineType=cv.LINE_AA)
@@ -56,6 +78,13 @@ def ajustaAltura(valor):
 def ajustaForca(valor):
     global d
     d = valor
+
+    if d == 0:
+        canvas[:, 481:961, :] = imagem_media
+    else:
+        efeito = tiltShift(linha_sup, linha_inf, d)
+        canvas[:, 481:961, :] = efeito
+    cv.imshow("Tilt Shift", canvas)
 
 # Ajusta posição vertical do desfoque
 def ajustaPosicao(valor):
@@ -76,7 +105,11 @@ def ajustaPosicao(valor):
         cv.imshow("Tilt Shift", canvas)
 
         canvas[:, 0:480, :] = imagem
-        canvas[:, 481:961, :] = 180
+        if d == 0:
+            canvas[:, 481:961, :] = imagem_media
+        else:
+            efeito = tiltShift(linha_sup, linha_inf, d)
+            canvas[:, 481:961, :] = efeito
 
         cv.line(canvas, (0, linha_sup), (im_w, linha_sup),
                 color=(0, 0, 255), thickness=1, lineType=cv.LINE_AA)
@@ -99,7 +132,11 @@ def ajustaPosicao(valor):
         cv.imshow("Tilt Shift", canvas)
 
         canvas[:, 0:480, :] = imagem
-        canvas[:, 481:961, :] = 180
+        if d == 0:
+            canvas[:, 481:961, :] = imagem_media
+        else:
+            efeito = tiltShift(linha_sup, linha_inf, d)
+            canvas[:, 481:961, :] = efeito
 
         cv.line(canvas, (0, linha_sup), (im_w, linha_sup), color=(0, 0, 255),
                 thickness=1, lineType=cv.LINE_AA)
@@ -107,13 +144,23 @@ def ajustaPosicao(valor):
                 thickness=1, lineType=cv.LINE_AA)
 
         cv.imshow("Tilt Shift", canvas)
-    return pivo
+
+# Função para produção do tilt shift e plotagem no canvas
+def tiltShift(ls, li, d):
+    a, um_menos_a = alfa(ls, li, d)
+
+    im_1 = np.multiply(a, imagem)
+    im_2 = np.multiply(um_menos_a, imagem_media)
+
+    return im_1 + im_2
+
+
 # Criação dos controles
 # Trackbar para tamanho da faixa de foco
 cv.createTrackbar("Janela de Foco", "Tilt Shift", 0, im_h_meio, ajustaAltura)
 
 # Trackbar para regular a força do decaimento
-cv.createTrackbar("Forca do decaimento(d)", "Tilt Shift", 0, 100, ajustaForca)
+cv.createTrackbar("Forca do decaimento(d)", "Tilt Shift", 0, 10, ajustaForca)
 
 # Trackbar para ajustar a posição vertical da janela de foco
 cv.createTrackbar("Posicao na Janela", "Tilt Shift", 180, im_h, ajustaPosicao)
@@ -121,7 +168,5 @@ cv.createTrackbar("Posicao na Janela", "Tilt Shift", 180, im_h, ajustaPosicao)
 ########################################################################
 # Programação
 ########################################################################
-print(linha_inf, linha_sup)
-
 cv.imshow('Tilt Shift', canvas)
 cv.waitKey(0)
