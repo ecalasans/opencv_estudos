c4058330ab68c583bfe03b74af7769f8df5bcbc5
ericcalasans <ericcalasans@gmail.com>
2020-11-26 04:55:08 -0300

    homomorfico.py - filtro funcionando corretamente

--- homomorfico.py
+++ homomorfico.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
+import cmath
 
 def gaussModif(gama_L, gama_H, c, D_0, imagem):
     # Variáveis de interesse
@@ -12,6 +13,7 @@ def gaussModif(gama_L, gama_H, c, D_0, imagem):
     v_c = im_w/2
 
     H = np.zeros(imagem.shape, dtype='float')
+    temp = np.zeros(imagem.shape, dtype='complex')
 
     # Matriz de coordenadas
     u, v = np.meshgrid(range(im_h), range(im_w), indexing='ij')
@@ -19,7 +21,7 @@ def gaussModif(gama_L, gama_H, c, D_0, imagem):
     # Etapas de cálculo do filtro gaussiano modificado
     D_uv_2 = (((u-u_c)**2 + (v-v_c)**2)**2).astype('float')
     div_D0 = D_uv_2/(D_0**2)
-    c_div_D0 = -c * div_D0
+    c_div_D0 = (-1)* c * div_D0
     exp_div_D0 = np.exp(c_div_D0)
     um_menos_exp = 1 - exp_div_D0
     mult_delta_gama = (gama_H - gama_L) * um_menos_exp
@@ -36,23 +38,65 @@ def zeroPadding(imagem):
     imagem_padded[0:cinza_h, 0:cinza_w] = imagem
     return imagem_padded
 
+def plotaFFT(imagem):
+    I_fft = np.fft.fft2(imagem)
+    I_shift = np.fft.fftshift(I_fft)
+    I_mag = np.abs(I_shift)
+    I_log = np.log(1+ I_mag)
+    I_int = np.uint8(I_log)
+    I_norm = cv.normalize(I_int, None, 0, 255, norm_type=cv.NORM_MINMAX)
+    cv.imshow("TF", I_norm)
+
 original = cv.imread('imagens/rxt.jpg')
 
 cinza = cv.cvtColor(original, cv.COLOR_BGR2GRAY)
 cinza_h = cinza.shape[0]
 cinza_w = cinza.shape[1]
 
-#  Zero padding para otimizar a DFT
-cinza_padded = zeroPadding(cinza)
-
-
 ########################################################################
 # Programação
 ########################################################################
 
-H_uv = gaussModif(
-    gama_L=0.4, gama_H=3.0, c=5, D_0=20, imagem=cinza_padded
-)
+#  Zero padding para otimizar a DFT
+cinza_padded = zeroPadding(cinza)
+plotaFFT(cinza_padded)
 
-cv.imshow("H_uv", H_uv)
+# Constrói o filtro
+H_uv = gaussModif(
+    gama_L= 0.9, gama_H=5, c=3, D_0=1000, imagem=cinza_padded
+)
+h = np.fft.fftshift(H_uv)
+cv.imshow("Filtro", H_uv)
+
+# Imagem no domínio logarítmico
+I_log = np.log(1 + cinza_padded)
+
+# DFT
+I_fft = np.fft.fft2(I_log)
+
+# Shift
+I_shifted = np.fft.fftshift(I_fft)
+# I_mag = np.abs(I_shifted)
+# I_int = np.uint8(I_mag)
+# I_norm = cv.normalize(I_int, None, 0, 255, cv.NORM_MINMAX)
+# cv.imshow("TF imagem Log", I_norm)
+
+# Filtragem
+I_filt = h * I_fft
+
+#  ITF
+I_inv_TF = np.fft.ifft2(I_filt)
+
+# Exponencial
+I_exp = np.exp(np.real(I_inv_TF)) - 1
+
+# Converte para uint8
+I_int = np.uint8(I_exp)
+
+
+# Normal e filtrada
+cv.imshow("Normal", cinza_padded)
+cv.imshow("Filtrada", I_int)
 cv.waitKey(0)
+cv.destroyAllWindows()
+
--- teste.py
+++ teste.py
@@ -2,7 +2,6 @@ import cv2 as cv
 import numpy as np
 
 a = np.ones((2,2))
-print((4-7)*a)
-
-
-
+f = np.subtract(3, a)
+j = 3 - a
+print(j)
